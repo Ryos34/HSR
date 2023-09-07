fa0f91d46ef2df8b9343177323db2312c7a0b690
Damiano <dbua96@gmail.com>
2023-09-07 23:31:00 +0200

    il mio primo commit

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+    $label = "CIAO MONDO";
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="it">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>HSR</title>
+</head>
+<body>
+    <?= $label; ?>
+</body>
+</html>
